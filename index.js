d95b7c99063edeb06e88d6d6c6a7311372c24e38
Mayabel <user@Retina.local>
2020-11-23 00:35:01 -0800

    new

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Calculate Postage
+app.get('/calculatePostage',getRate )
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+/*
+ Functions for the Postage Calculations
+*/
+function getRate(req, res) {
+
+	console.log("getting postage");
+
+	const weight = req.query.weight;
+	const mailType = req.query.mailType;
+
+	const price = calculateRate(weight, mailType);
+
+	params = {
+		weight: weight,
+		mailType: mailType,
+		price: price,
+	}
+
+	res.render('pages/displayRate', params);
+}
+
+function calculateRate(weight, mailType) {
+	console.log("calculating postage");
+
+	switch(mailType) {
+		case "stamped":
+			console.log("postage: stamped");
+			return 0.49 + (Number(weight) - 1) * 0.21;
+			break;
+		case "metered":
+			console.log("postage: metered");
+			return 0.46 + (Number(weight) - 1) * 0.21;
+			break;
+		case "largeEvn":
+			console.log("postage: largeEnv");
+			return 0.98 + (Number(weight) - 1) * 0.21;
+			break;
+		case "parcel":
+			console.log("postage: parcel");
+			if (Number(weight) >= 4) {
+				return 2.67 + (Number(weight) - 4) * 0.18;
+			}
+			else {
+				return 2.67;
+			} 
+			break;
+		default:
+			console.log("postage deafault");
+	}
+}
